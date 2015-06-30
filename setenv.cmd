862e289f795340d36144ea57f4a66514be1bf488
ColinA-MSFT <colina@microsoft.com>
2015-06-30 15:10:15 -0700

    Minor fixes to scripts and make files.

--- setenv.cmd
+++ setenv.cmd
@@ -1,31 +1,22 @@
 @echo off
 
-
-
 set ZELIG_ROOT=%~dp0%
 
 prompt $_$+---$G $P$_$+   $G$_Cmd$G
 
-REM 
-REM Set LLVM environment variables
-REM 
+::
+:: Set LLVM environment variables.
+::
 
 set LLVM_BIN=%ZELIG_ROOT%external\LLVM\Debug\bin\
-
 set LLVM_INCLUDE=%ZELIG_ROOT%external\LLVM\include\
-
-
 set LLVM_LIBS=%ZELIG_ROOT%external\LLVM\
 
+::
+:: Add pyOCD and and make to path.
+::
 
-
-REM 
-REM Add pyOCD and and make to path
-REM 
 set PATH=%ZELIG_ROOT%external\tools;%PATH%
 
-
-
-cd zelig
-
-echo Welcome to Zelig! 
+cd Zelig
+echo Welcome to LLILUM! 
