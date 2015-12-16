a93f6981fa72e728bacb723ebff4752c320c171a
Mikhail Skobov <mikhas@microsoft.com>
2015-12-16 10:09:15 -0800

    Adding a TCP/IP stack sample for desktop and device under <repo>\Zelig\Zelig\Test\Desktop\Net\NetworkTest and <repo>\Zelig\Zelig\Test\mbed\SimpleNet

---
+++ Zelig/Zelig/Test/Desktop/Net/NetworkTest/Program.cs
@@ -0,0 +1,129 @@
+﻿//
+// Copyright (c) Microsoft Corporation.    All rights reserved.
+//
+
+namespace Microsoft.Zelig.Test
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SynchronousSocketListener
+    {
+        // Incoming data from the client.
+        public static string data = null;
+
+        public static void StartListening( )
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // Dns.GetHostName returns the name of the 
+            // host running the application.
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+
+            IPAddress ipv4Local = null;
+            foreach( var addr in ipHostInfo.AddressList)
+            {
+                if(addr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipv4Local = addr;
+
+                    break;
+                }
+            }
+
+            IPHostEntry ipHostInfo2 = Dns.GetHostEntry("bing.com");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipv4Local, 11000);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and 
+            // listen for incoming connections.
+            try
+            {
+                listener.Bind( localEndPoint );
+                listener.Listen( 10 );
+                Socket handler = null;
+
+
+                // Start listening for connections.
+                while(true)
+                {
+                    Console.WriteLine( "Waiting for a connection..." );
+
+                    handler = listener.Accept( );
+
+                    Console.WriteLine( "... incoming connection for socket:" + handler.LocalEndPoint.ToString( ) );
+
+                    Task.Run( new Action( delegate
+                    {
+                        handler.ReceiveTimeout = 10 * 1000; // ten seconds timeout
+                        
+                        int count = 0;
+
+                        while(++count <= 10)
+                        {
+                            // An incoming connection needs to be processed.
+                            while(true)
+                            {
+                                Console.WriteLine( String.Format( "Performing read #{0:D2} from '{1}'", handler.LocalEndPoint.ToString( ) ) ); 
+
+                                bytes = new byte[ 1024 ];
+
+                                int bytesRec = handler.Receive(bytes);
+
+                                Console.WriteLine( "... read " + bytesRec + " bytes" );
+
+                                var moreData = Encoding.ASCII.GetString( bytes, 0, bytesRec );
+
+                                Console.WriteLine( "received data: " + moreData );
+
+                                data += moreData;
+
+                                if(data.IndexOf( "\n" ) > -1)
+                                {
+                                    break;
+                                }
+                            }
+
+                            // Show the data on the console.
+                            Console.WriteLine( "Text received : {0}", data );
+
+                            // Echo the data back to the client.
+                            byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                            Console.WriteLine( "... sending to: " + handler.LocalEndPoint.ToString( ) );
+
+                            handler.Send( msg );
+                        }
+
+                        handler.Close( );
+                    } )
+                    );
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine( e.ToString( ) );
+            }
+
+            Console.WriteLine( "\nPress ENTER to continue..." );
+            Console.Read( );
+
+        }
+
+        public static int Main( String[] args )
+        {
+            StartListening( );
+            return 0;
+        }
+
+    }
+}
---
+++ Zelig/Zelig/Test/mbed/SimpleNet/Program.cs
@@ -0,0 +1,83 @@
+﻿//
+// Copyright (c) Microsoft Corporation.    All rights reserved.
+//
+
+#define K64F
+
+namespace Microsoft.Zelig.Test.mbed.SimpleNet
+{
+    using System.Net.Sockets;
+    using System.Net;
+    using System.Threading;
+    using Microsoft.Llilum.Lwip;
+    
+    using System.Text;
+
+    class Program
+    {
+        static void Main( )
+        {
+            NetworkInterface netif = NetworkInterface.GetAllNetworkInterfaces()[0];
+            netif.EnableDhcp( );
+            //netif.EnableStaticIP("10.125.148.136", "255.255.254.0", "10.125.148.1");
+
+            //IPHostEntry entry = Dns.GetHostEntry("bing.com");
+
+            //IPAddress dnsAddress = entry.AddressList[0];
+
+            //byte[] dnsCmd = new byte[entry.HostName.Length];
+
+            //for(int i = 0; i < entry.HostName.Length; i++)
+            //{
+            //    dnsCmd[ i ] = (byte)entry.HostName[ i ];
+            //}
+
+            while(true)
+            {
+                Socket sock = new Socket(AddressFamily.Unspecified, SocketType.Stream, ProtocolType.Unspecified);
+
+                //byte[] address = new byte[] {(byte)217, (byte)140, (byte)101, (byte)20};
+                //IPEndPoint endPoint = new IPEndPoint(new IPAddress((long)0x14658CD9), 80);
+                IPEndPoint endPoint = new IPEndPoint( IPAddress.Parse( "10.125.148.170" ), 11000);
+
+                sock.Connect( endPoint );
+
+                string command = "GET /media/uploads/mbed_official/hello.txt HTTP/1.0\n\n";
+
+                byte[] byteCmd = new byte[] {
+                                        (byte)'G',
+                                        (byte)'E',
+                                        (byte)'T',
+                                        (byte)' ',
+                                        (byte)'/',
+                                        (byte)' ',
+                                        (byte)'H',
+                                        (byte)'T',
+                                        (byte)'T',
+                                        (byte)'P',
+                                        (byte)'/',
+                                        (byte)'1',
+                                        (byte)'.',
+                                        (byte)'0',
+                                        0xa,
+                                        0xa,
+                                    };
+
+                int count = 0;
+
+                var recBuffer = new byte[512];
+
+                while(++count <= 10)
+                {
+                    if(sock.Send( byteCmd ) > 0)
+                    //if (sock.Send(ASCIIEncoding.ASCII.GetBytes(command)) > 0)
+                    {
+                        sock.Receive( recBuffer );
+                    }
+                }
+
+                sock.Close( );
+            }
+        }
+    }
+}
