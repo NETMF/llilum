09211a95595b4b0f1977734003667d70ae1df18a
Mikhail Skobov <mikhas@microsoft.com>
2015-12-15 19:03:49 -0800

    Changing heap size to 24K for LPC1768

--- Zelig/LLVM2IR_results/mbed/simple/build.bat
+++ Zelig/LLVM2IR_results/mbed/simple/build.bat
@@ -18,14 +18,14 @@ IF /i "%GCC_BIN%"=="" (
 IF %1.==. (
     ECHO No target passed in. Defaulting to LPC1768
     set TARGET=LPC1768
-    set SIZE_OF_HEAP=0x6B00
+    set SIZE_OF_HEAP=0x6000
 ) ELSE (
     ECHO Detected target: %1
     set TARGET=%1
     IF "%1"=="K64F" (
     set SIZE_OF_HEAP=0x10000
     ) ELSE (
-    set SIZE_OF_HEAP=0x6B00
+    set SIZE_OF_HEAP=0x6000
     )
 )
 
