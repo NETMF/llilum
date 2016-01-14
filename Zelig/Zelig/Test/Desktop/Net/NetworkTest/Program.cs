32b57ef7844b4373d9ff87dc4d2b939efc6b3e92
lorenzte@ntdev.microsoft.com <lorenzte@microsoft.com>
2016-01-13 19:12:23 -0800

    Network test sample app improvements

--- Zelig/Zelig/Test/Desktop/Net/NetworkTest/Program.cs
+++ Zelig/Zelig/Test/Desktop/Net/NetworkTest/Program.cs
@@ -1,4 +1,7 @@
-﻿//
+﻿
+
+
+//
 // Copyright (c) Microsoft Corporation.    All rights reserved.
 //
 
@@ -9,6 +12,7 @@ namespace Microsoft.Zelig.Test
     using System.Net;
     using System.Net.Sockets;
     using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
 
     public class SynchronousSocketListener
@@ -81,79 +85,83 @@ namespace Microsoft.Zelig.Test
                 //
                 // Start listening for connections.
                 //
-                int connection    = 1;
+                int connection = 1;
                 while(true)
                 {
-                    Console.WriteLine( String.Format( "========= Waiting on {0} for connection #{1} =========", localEndPoint.ToString( ), connection++ ) );
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = NextColor;
+                    Console.Write( String.Format( "Waiting on {0} for connection #{1}... ", localEndPoint.ToString( ), connection++ ) );
 
                     handler = listener.Accept( );
 
-                    Console.WriteLine( String.Format( " => => => => => => incoming connection from {0} !", handler.LocalEndPoint.ToString( ) ) );
+                    Console.Write( String.Format( "Connected to {0}\r\n", handler.LocalEndPoint.ToString( ) ) );
 
                     //Task.Run( new Action( delegate
                     //{
-                        try
+                    try
+                    {
+                        handler.ReceiveTimeout = 120 * 1 * 1000; // ten seconds timeout
+
+                        int count = 0;
+
+                        var buffer = new byte[ 1024 ];
+
+                        // An incoming connection needs to be processed.
+                        while(true)
                         {
-                            handler.ReceiveTimeout = 10 * 1000; // ten seconds timeout
+                            ++count;
 
-                            int count = 0;
+                            Console.Write( String.Format( "Performing read #{0:D4} from '{1}'. Bytes Received so far: {2:D8}\r",
+                                count,
+                                handler.RemoteEndPoint.ToString( ),
+                                bytesReceived
+                                ) );
 
-                            var buffer = new byte[ 1024 ];
+                            int bytesRead = handler.Receive(buffer);
 
-                            // An incoming connection needs to be processed.
-                            while(true)
+                            bytesReceived += bytesRead;
+
+                            var moreData = Encoding.ASCII.GetString( buffer, 0, bytesRead );
+
+                            builder.Append( moreData );
+
+                            string finalString = builder.ToString();
+
+                            // Ensure we got the return char terminated request
+                            if(finalString.IndexOf( "\n" ) != -1)
                             {
-                                ++count;
+                                builder.Clear( );
 
-                                Console.WriteLine( String.Format( "Performing read #{0:D4} from '{1}'. Bytes Received so far: {2}",
+                                data.Add( finalString );
+                            }
+
+                            //
+                            // Echo data back
+                            //
+                            handler.Send( buffer, 0, bytesRead, SocketFlags.None );
+
+                            if(finalString.IndexOf( "TEST_COMPLETED" ) != -1)
+                            {
+                                Console.Write( String.Format( "Performing read #{0:D4} from '{1}'. Bytes Received so far: {2:D8}\r\n",
                                     count,
                                     handler.RemoteEndPoint.ToString( ),
                                     bytesReceived
                                     ) );
 
-                                int bytesRead = handler.Receive(buffer);
-                                Console.WriteLine(string.Format("Received: {0} bytes", bytesRead));
-
-                                bytesReceived += bytesRead;
-
-                                var moreData = Encoding.ASCII.GetString( buffer, 0, bytesRead );
-
-                                builder.Append(moreData);
-
-                                string finalString = builder.ToString();
-
-                                // Ensure we got the return char terminated request
-                                if (finalString.IndexOf("\n") != -1)
-                                {
-                                    builder.Clear();
-                                    data.Add(finalString);
-                                    Console.WriteLine(String.Format("Received: {0}", finalString));
-                                }
-                                
-                                //
-                                // Echo data back
-                                //
-                                handler.Send( buffer, 0, bytesRead, SocketFlags.None );
-
-                                if (finalString.IndexOf("TEST_COMPLETED") != -1)
-                                {
-                                    Console.WriteLine("");
-                                    Console.WriteLine(String.Format("<= <= <= <= <= <= {0} completed", handler.RemoteEndPoint.ToString()));
-
-                                    break;
-                                }
+                                break;
+                            }
                         }
-                        }
-                        catch(Exception ex)
-                        {
-                            Console.WriteLine( "" );
-                            Console.WriteLine( String.Format( "<= <= <= <= <= <= EXCEPTION CAUGHT on {0}", handler.RemoteEndPoint.ToString() ) );
-                            Console.WriteLine( ex.StackTrace );
-                        }
-                        finally
-                        {
-                            handler.Close( );
-                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine( "" );
+                        Console.WriteLine( String.Format( "EXCEPTION CAUGHT on {0}", handler.RemoteEndPoint.ToString( ) ) );
+                        Console.WriteLine( ex.StackTrace );
+                    }
+                    finally
+                    {
+                        handler.Close( );
+                    }
                     //} )
                     //);
                 }
@@ -167,6 +175,18 @@ namespace Microsoft.Zelig.Test
             Console.Read( );
 
             return bytesReceived;
+        }
+
+        //--//
+
+        static readonly ConsoleColor[] s_colors = new ConsoleColor[] { ConsoleColor.White, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Cyan };
+        static int s_currentColorIndex = -1;
+        public static ConsoleColor NextColor
+        {
+            get
+            {
+                return s_colors[ Interlocked.Increment( ref s_currentColorIndex ) % s_colors.Length ];
+            }
         }
 
         //--//
