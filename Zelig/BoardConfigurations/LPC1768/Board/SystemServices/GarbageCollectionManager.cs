7eac0008422414e779fd4642802d37155bec4c36
Mikhail Skobov <mikhas@microsoft.com>
2016-02-05 08:25:27 -0800

    Updating all of the FrontEndConfig files for the SDK and BoardConfiguration solution to match those in the main Zelig solution and removing redundant GC schemes from LPC1768 BSP

--- Zelig/BoardConfigurations/LPC1768/Board/SystemServices/GarbageCollectionManager.cs
+++ Zelig/BoardConfigurations/LPC1768/Board/SystemServices/GarbageCollectionManager.cs
@@ -2,21 +2,37 @@
 // Copyright (c) Microsoft Corporation.    All rights reserved.
 //
 
+// The default GC scheme will be Conservative Mark and Sweep.
+// Only one of these should be enabled at a time
+
+#define MarkAndSweepGC
+
 namespace Microsoft.Llilum.LPC1768
 {
     using Chipset = Microsoft.CortexM3OnMBED;
 
+#if ReferenceCountingGC
 
     public sealed class ReferenceCountingCollector : Microsoft.Zelig.Runtime.ReferenceCountingCollector
     {
     }
 
+#endif
+
+#if StrincReferenceCountingGC
+
     public sealed class StrictReferenceCountingCollector : Microsoft.Zelig.Runtime.StrictReferenceCountingCollector
     {
     }
 
+#endif
+
+#if MarkAndSweepGC
+
     public sealed class ConservativeMarkAndSweepCollector : Microsoft.Zelig.Runtime.ConservativeMarkAndSweepCollector
     {
     }
+
+#endif
 }
 
