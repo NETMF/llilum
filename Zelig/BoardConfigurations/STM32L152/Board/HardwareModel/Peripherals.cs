3cd4e3a141213b98dad3894721b925e9b9fc7e53
lt72 <lorenzte@microsoft.com>
2016-03-01 19:48:10 -0800

    Fixes #195

--- Zelig/BoardConfigurations/STM32L152/Board/HardwareModel/Peripherals.cs
+++ Zelig/BoardConfigurations/STM32L152/Board/HardwareModel/Peripherals.cs
@@ -2,12 +2,15 @@
 // Copyright (c) Microsoft Corporation.    All rights reserved.
 //
 
+//#define ALLOW_PAUSE
+
+
 namespace Microsoft.Llilum.STM32L152
 {
     using Chipset = Microsoft.CortexM3OnMBED;
 
 
-    public sealed class Device : Chipset.Device
+    public sealed class Peripherals : Chipset.Peripherals
     {
     }
 }
